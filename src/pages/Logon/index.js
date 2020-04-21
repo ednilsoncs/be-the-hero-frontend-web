6e370589652d4be0858b1043d0be6a9c2a8b0679
ednilsonCS <ednilsoncs@dcomp.ufs.br>
2020-04-21 14:00:38 -0300

    Front-end do app

---
+++ src/pages/Logon/index.js
@@ -0,0 +1,41 @@
+import React, {  useState } from 'react';
+import { FiLogIn } from 'react-icons/fi'
+import herosImg from '../../assets/heroes.png';
+import logoImg from '../../assets/logo.svg';
+import api from '../../services/api'
+import './styles.css'
+import { Link, useHistory } from 'react-router-dom';
+
+export default function Logon(){
+  const  [id,setId] = useState();
+  const history = useHistory();
+  const handleLogin = async(e)=>{
+     e.preventDefault();
+     try{
+      const { data } = await api.post('/sessions',{ id })  
+      localStorage.setItem('ongName',data.name);
+      localStorage.setItem('ongId',id);
+      history.push('/profile');
+    }catch(e){
+      alert("Falha no login")
+     }
+  }
+  return(
+    <div className="logon-container">
+      <section className="form">
+        <img src={logoImg} alt="Be the Hero"/>
+        <form onSubmit={handleLogin}>
+           <h1>Faça seu logon</h1>
+           <input 
+             placeholder="Sua ID"
+             value={id}
+             onChange={e=>setId(e.target.value)}
+             />
+           <button className="button" type="submit">Entrar</button>
+           <Link className="back-link" to="/register"><FiLogIn size={16} color="#E02041"/> Não tenho cadastro</Link>
+        </form>
+      </section>
+      <img src={herosImg} alt="Heros"/>
+    </div>
+  )
+};
---
+++ src/pages/NewIncident/index.js
@@ -0,0 +1,60 @@
+import React,{useState} from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi'
+import logoImg from '../../assets/logo.svg';
+
+import './styles.css'
+import api from '../../services/api';
+
+export default function NewIncident(){
+  const  [title,setTitle]= useState('')
+  const  [description,setDescription]= useState('')
+  const  [value,setValue]= useState('');
+  const  history = useHistory();
+  const ongId = localStorage.getItem('ongId');
+  const handleNewIncident = async(e)=>{
+    e.preventDefault();
+    const body = {
+      title,
+      description,
+      value,
+    }
+    try{
+     await api.post('incidents',body,{
+       headers:{
+         Authorization:ongId
+       }
+     });
+     history.push('/profile');
+    }catch(e){
+      alert('Erro ao cadastrar caso, tente novamente')
+    }
+  }
+ return(
+   <div className="new-incident-container">
+      <div className="content">
+      <section>
+        <img src={logoImg}></img>
+        <h1>Cadastrar novo caso</h1>
+        <p>Descreva o caso detalhadamente para encontrar um novo herói para resolver isso.</p>
+        <Link className="back-link" to="/profile"><FiArrowLeft size={16} color="#E02041"/> Voltar para a home</Link>
+      </section>
+      <form onSubmit={handleNewIncident}>
+        <input 
+         value={title}
+         onChange={e=>setTitle(e.target.value)}
+         placeholder="Título do caso "/>
+        <textarea 
+         value={description}
+         onChange={e=>setDescription(e.target.value)}
+         placeholder="Descrição"/>
+        <input 
+         value={value}
+         onChange={e=>setValue(e.target.value)}
+         placeholder="Valor em reais"/>
+        <button className="button" type="submit">Cadastrar</button>
+      </form>
+      </div>
+   </div>
+ )
+}
