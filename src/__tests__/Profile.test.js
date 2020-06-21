92bc0b803c97911ce4bcccb2cb323a2fb261be42
ednilsoncs <ednilsoncs@dcomp.ufs.br>
2020-06-21 01:15:29 -0300

    project: added fleg jest in eslint configuration

--- src/__tests__/Profile.test.js
+++ src/__tests__/Profile.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, fireEvent, act, getByText } from '@testing-library/react';
+import { render, fireEvent, act } from '@testing-library/react';
 import MockAdapter from 'axios-mock-adapter';
 import { MemoryRouter } from 'react-router-dom';
 import api from '../services/api';
